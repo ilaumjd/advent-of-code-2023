e7f5b816fa606cbd388d675356daac3ae527d86c
ilaumjd <ilaumjd@gmail.com>
2023-12-11 08:38:48 +0700

    day 9

---
+++ 09a.rb
@@ -0,0 +1,20 @@
+total = 0
+File.readlines('09in').each do |line|
+  nums = line.split(' ').map(&:to_i)
+
+  seq = []
+  nums.each do |num|
+    next_seq = [num]
+    pointer = 0
+    while seq.count >= next_seq.count
+      break if next_seq[pointer].zero? && seq[pointer].zero?
+
+      next_seq << next_seq[pointer] - seq[pointer]
+      pointer += 1
+    end
+    seq = next_seq
+  end
+  total += seq.sum
+end
+
+puts total
